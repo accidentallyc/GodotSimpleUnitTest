7696c8d8da3e11c07f845be5d1405cbfb6a38141
CasseyCola <project.cassey@gmail.com>
2023-12-31 08:50:16 +1300

    Merged v0.0.4

--- addons/godot_simple_test/sample/test_suite_2.gd
+++
@@ -1,7 +0,0 @@
-extends SimpleTest
-
-func test_can_have_multiple_suites():
-	pass
-
-func test_can_fail():
-	expect("foo").to.equal("bar")
--- addons/godot_simple_test/src/ui/SimpleTest_Canvas.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-class_name SimpleTest_Canvas
-
-@export var container:Container
-
-func add_block(block:Control):
-	container.add_child.call_deferred(block)
